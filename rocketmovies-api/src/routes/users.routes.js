999c22a44c75314b69624bca2ab41fd5093cb2c5
lucasdmmc <lucas.dmmc@hotmail.com>
2022-12-12 22:32:09 -0300

    Finalizei autentação do usário

---
+++ rocketmovies-api/src/routes/users.routes.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+
+const usersRoutes = Router();
+const UserController = require("../controllers/UserController")
+const userController = new UserController();
+
+usersRoutes.post("/", userController.create)
+usersRoutes.put("/:id", userController.update)
+
+module.exports = usersRoutes;
