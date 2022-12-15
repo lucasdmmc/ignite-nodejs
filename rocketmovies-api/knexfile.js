dbe19b5dba48a2394e68edccd87f9f14b36610ce
lucasdmmc <lucas.dmmc@hotmail.com>
2022-12-14 23:56:54 -0300

    Descomentei uma parte do codigo para teste

--- rocketmovies-api/knexfile.js
+++ rocketmovies-api/knexfile.js
@@ -7,9 +7,9 @@ module.exports = {
       filename: path.resolve(__dirname, "src", "database", "database.db")
     },
     useNullAsDefaul: true,
-    // pool: {
-    //   afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb)
-    // },
+    pool: {
+      afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb)
+    },
     migrations: {
       directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
     },
